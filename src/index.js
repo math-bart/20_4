e565d88e2efe6b0aa41e5e46f16a0dc3c798290b
Bartosz Wojtaś <el.klides@op.pl>
2018-04-22 11:42:41 +0200

    Create project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
+import DevTools from './DevTools';
+import routes from './routes';
+
+render(
+    <Provider store={store}>
+	  <div>
+         <Router history={hashHistory} routes={routes}/>
+		<DevTools />
+	  </div>
+    </Provider>,
+    document.getElementById('root')
+);
